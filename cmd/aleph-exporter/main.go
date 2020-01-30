7a37c394305b0574403f300317db1f38a1b5c2ea
Christoph Klünter <c.klunter@ndr.de>
2020-01-30 16:23:23 +0100

    allow to configure exporter via env variables

--- cmd/aleph-exporter/main.go
+++ cmd/aleph-exporter/main.go
@@ -7,28 +7,36 @@ import (
 	"github.com/ckluenter/aleph-exporter/pkg/web"
 	"github.com/facebookgo/grace/gracehttp"
 	"net/http"
+	"os"
 	"time"
 )
 
 var (
-	addr          = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-	alephHost     = flag.String("aleph-host", "localhost", "hostname of aleph")
-	alephToken    = flag.String("aleph-token", "asdfasdfasdfasdf", "token to be used for authentication")
-	SkipTLSVerify = flag.Bool("skip-tls-verify", false, "Set to true to not verify the authenticity the server's TLS certificate.")
+	port          = flag.String("listen-port", env("ALEPH_LISTEN_PORT"), "The port to listen on for HTTP requests. e.g. :8080")
+	alephHost     = flag.String("aleph-host", env("ALEPH_HOST"), "hostname of aleph")
+	alephToken    = flag.String("aleph-token", env("ALEPH_TOKEN"), "token to be used for authentication")
+	SkipTLSVerify = flag.Bool("skip-tls-verify",envBool("ALEPH_SKIP_TL_VERIFY"), "Set to true to not verify the authenticity the server's TLS certificate.")
 )
 
 func main() {
+	if *port == "" {
+		*port = ":8080"
+	}
+
+	if *alephHost == "" {
+		*alephHost = "localhost"
+	}
 	flag.Parse()
 	r := web.NewRouter()
 	r = observe.RegisterPrometheus(r)
 	srv := &http.Server{
-		Addr:         *addr,
+		Addr:         *port,
 		Handler:      r,
 		ReadTimeout:  0,
 		WriteTimeout: 0,
 	}
 
-	fmt.Printf("aleph exporter started. Listening on %s and exposing api from %s \n\n", *addr, alephUrl(*alephHost))
+	fmt.Printf("aleph exporter started. Listening on %s and exposing api from %s \n\n", *port, alephUrl(*alephHost))
 	go func() {
 		for {
 			requestBody := observe.GetAlephStatus(alephUrl(*alephHost), *alephToken, true)
@@ -44,4 +52,20 @@ func main() {
 
 func alephUrl(hostName string) string {
 	return "https://" + hostName + "/api/2/status"
+}
+
+func env(name string) string {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		return ""
+	}
+	return value
+}
+
+func envBool(name string) bool {
+	value, ok := os.LookupEnv(name)
+	if !ok {
+		return false
+	}
+	return value == "false"
 }
